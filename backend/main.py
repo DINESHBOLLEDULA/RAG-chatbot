5e75fb6c1aa228b7c02878127a92b1417d5d33a5
DINESHBOLLEDULA <dineshbolledula@gmail.com>
2025-12-03 14:13:51 +0530

    changed cors

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
-app.add_middleware(CORSMiddleware,allow_origins=['https://rag-chatbot-hazel.vercel.app'])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://rag-chatbot-hazel.vercel.app",   # ⬅ NO trailing slash
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
